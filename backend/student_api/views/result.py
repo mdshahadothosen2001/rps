345982c7b3516f22f12bf675570a1440c1c434da
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-06-28 23:21:09 +0600

    chore: result api modified for final

--- backend/student_api/views/result.py
+++ backend/student_api/views/result.py
@@ -4,7 +4,7 @@ from django.db.models import Sum, Count
 from django.shortcuts import get_object_or_404, get_list_or_404
 
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
 from user.models import UserAccount
@@ -15,27 +15,45 @@ from utils.utils import tokenValidation
 
 
 class SemesterResultView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def get(self, request):
-        payload = tokenValidation(request)
-        student_id = payload.get("id")
+        registration_no = request.query_params.get("registration_no")
         semester_id = request.query_params.get("semester_id")
-        student_data = get_object_or_404(UserAccount, id=student_id)
-        result = get_object_or_404(GPA, student__id=student_id, semester__id=semester_id)
-        marks = get_list_or_404(Mark, examination__semester__id=semester_id, examination__name="final")
-        marks_serializer = MarkEachCourseSerializer(marks, many=True)
-        serializer = SemesterResultSerializer(result)
-        result = {
-            "first_name": student_data.first_name,
-            "last_name": student_data.last_name,
-            "registration": student_data.username,
-            "roll": student_data.roll,
-            "GPA": serializer.data.get("point"),            
-            "course_wise": marks_serializer.data,
-            "date": datetime.now().date()
-        }
-        return Response(result)
+        if semester_id not in ["FINAL", "Final", "final"]:
+            student_data = get_object_or_404(UserAccount, username=registration_no)
+            result = get_object_or_404(GPA, student__username=registration_no, semester__id=semester_id)
+            marks = get_list_or_404(Mark, examination__semester__id=semester_id, examination__name="final")
+            marks_serializer = MarkEachCourseSerializer(marks, many=True)
+            serializer = SemesterResultSerializer(result)
+            result = {
+                "first_name": student_data.first_name,
+                "last_name": student_data.last_name,
+                "registration": student_data.username,
+                "roll": student_data.roll,
+                "GPA": serializer.data.get("point"),            
+                "course_wise": marks_serializer.data,
+                "date": datetime.now().date()
+            }
+            return Response(result)
+        else:
+            student_data = get_object_or_404(UserAccount, username=registration_no)
+            result = GPA.objects.filter(student__username=registration_no)
+            total_point = result.aggregate(total_point=Sum("point"))["total_point"]
+            total_passed_semester = result.aggregate(total_passed_semester=Count("id"))["total_passed_semester"]
+            serializer = SemesterResultSerializer(result, many=True)
+            if total_passed_semester == 8:
+                result = {
+                    "first_name": student_data.first_name,
+                    "last_name": student_data.last_name,
+                    "registration": student_data.username,
+                    "roll": student_data.roll,
+                    "CGPA": total_point/8,            
+                    "semester_wise": serializer.data,
+                    "date": datetime.now().date()
+                }
+                return Response(result)
+            return Response("not found")
 
 
 class SemesterFinalResultView(APIView):
